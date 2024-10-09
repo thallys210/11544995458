03d2d0af0680d245ec04c46ae93fd5196e6a6e7f
thallys210 <thallysandrey80@gmail.com>
2024-10-09 14:48:29 -0300

    .

---
+++ AULA-JS/js/index.js
@@ -0,0 +1,21 @@
+/* Vamos entender Variaveis
+Variaveis são "recipientes" onde podemos armazenar infromações que 
+podem variar, ou seja, podem ter qualquer tipo de valor.
+
+No javascript temos 3 palavras-chaves para declarar variaveis:
+-> var
+-> let
+-> cpmst
+*/
+
+//DECLARAÇÃO DE VARIAVEIS
+var a, b, c;
+
+//ATRIBUIÇÃO DOS VALORES
+var a = 2;
+b = 3;
+ c = a + b;
+
+ var a=5;
+ 
+alert(c);
