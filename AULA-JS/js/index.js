dab664e5dd6b80b629120fffcc7fc15e359763c6
thallys210 <thallysandrey80@gmail.com>
2024-10-24 13:03:08 -0300

    .

--- AULA-JS/js/index.js
+++ AULA-JS/js/index.js
@@ -1,1 +1,17 @@
+const carro = {
+    marca: "ford", 
+    modelo: "ka", 
+    ano:2015, 
+    placa: "ABC-1234",
+    buzina: function () { alert('Biiiiiiiiiii') },
+   completo: function(){
+    return "Amarca é " + this.marca + " e o modelo e e: " + this.modelo
+   }
+ };
 
+console.log(carro.completo());
+
+
+function eventoClick() {
+  alert('acionou um evento de lick');
+}
